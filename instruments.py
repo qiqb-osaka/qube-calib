d72ccd7e472da53f6762f14d871ccf38a96b4d1a
h403 <shiomi@qiqb.osaka-u.ac.jp>
2022-04-06 10:48:22 +0900

    Changed data container type returnd by MS2038_SPA.measure method

--- instruments.py
+++ instruments.py
@@ -2,14 +2,11 @@ import pyvisa
 import time
 import numpy as np
 
-m = pyvisa.ResourceManager('@py')
-inst = m.open_resource("TCPIP0::10.250.0.10::inst0::INSTR")
-inst.query('*IDN?')
-
 def new_resource_manager(interface_type='@py'):
     return pyvisa.ResourceManager(interface_type)
 
 class MS2038_SPA(object):
+    Contents = namedtuple('Contents', ('preamble', 'trace'))
     def __init__(self, manager, resource='TCPIP0::10.250.0.10::inst0::INSTR'):
         self.instrument = manager.open_resource(resource)
     def query(self, v):
@@ -35,12 +32,10 @@ class MS2038_SPA(object):
         return self.instrument.query(':TRAC?')
     def measure(self):
         self.initiate()
-        t = self.get_trace()
-        p = self.get_preamble()
-        return {
-            'trace': t,
-            'preamble': p,
-        }
+        return MS2038_SPA.Contents(
+            trace = self.get_trace(),
+            preamble =  self.get_preamble(),
+        )
     @classmethod
     def convert_trace_to_array(cls, trace):
         return np.loadtxt(trace[6:].split(','))
@@ -53,25 +48,3 @@ class MS2038_SPA(object):
         fstop = myconv(myfind('STOP_FREQ', p))
         npts = int(float(myfind('UI_DATA_POINTS', p).split('=')[1]))
         return np.linspace(fstart, fstop, npts)
-
-# # MS2038Cから測定データを読み出す
-# def get_freq_from_ms2038c(inst):
-#     myfind = lambda k, l: next(filter(lambda x: x.startswith("{}=".format(k)), l))
-#     myconv = lambda s: float(s.split('=')[1].replace(' MHZ', ''))
-#     inst.timeout = 10000
-#     preamble = inst.query(':TRAC:PRE?')[10:].split(',')
-#     fstart = myconv(myfind('START_FREQ', preamble))
-#     fstop = myconv(myfind('STOP_FREQ', preamble))
-#     npts = int(float(myfind('UI_DATA_POINTS', preamble).split('=')[1]))
-#     return np.linspace(fstart, fstop, npts)
-
-# def get_trace_from_ms2038c(inst):
-#     return np.loadtxt(inst.query(':TRAC?')[6:].split(','))
-
-# def sweep_ms2038c(inst):
-#     inst.write(':INIT')
-#     while True:
-#         time.sleep(.1)
-#         if inst.query(':STAT:OPER?') == '256':
-#             break
-#     return True
