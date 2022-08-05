29a732b3341ead4214f6f11c4cebbbf40ba3f477
h403 <shiomi@qiqb.osaka-u.ac.jp>
2022-08-05 12:02:31 +0900

    add qubecalib/pulse, qubecalib/setup

---
+++ qubecalib/qubecalib/pulse/__init__.py
@@ -0,0 +1,17 @@
+class Schedule(object):
+    
+    def __init__(self, channels):
+        pass
+    
+    def initiate(self):
+        pass
+    
+class ChannelBase(object):
+    pass
+
+class TxChannel(ChannelBase):
+    pass
+
+class RxChannel(ChannelBase):
+    pass
+
