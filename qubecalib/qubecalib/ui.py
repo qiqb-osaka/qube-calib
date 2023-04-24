e8260889a875ccbae6bf2cca09693825586bb110
Hidehisa SHIOMI <shiomi.hidehisa.qiqb@osaka-u.ac.jp>
2023-04-24 16:17:38 +0900

    Fix ui.BootUnitButton

--- qubecalib/qubecalib/ui.py
+++ qubecalib/qubecalib/ui.py
@@ -212,7 +212,7 @@ def show_status(qube):
 
 def boot_fpga_from_rom(qube):
     os.environ['ADAPTER'] = qube.adapter_au50
-    ROOT = lib_qube.QUBECALIB_PATH_TO_ROOT
+    ROOT = lib_qube.PATH_TO_ROOT
     cmd = 'vivado -mode batch -source {}/../qube_multi/qube_client/tools/reboot_from_rom.tcl'.format(ROOT).split(' ')
     ret = subprocess.check_output(cmd, encoding='utf-8')
     return ret
