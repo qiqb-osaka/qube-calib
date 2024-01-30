7a3048a2a8425fa83714e72dd4b30562b0885363
Hidehisa SHIOMI <shiomi.hidehisa.qiqb@osaka-u.ac.jp>
2024-01-30 09:17:42 +0900

    Improve import method for Unit

--- qubecalib/qubecalib/units.py
+++ qubecalib/qubecalib/units.py
@@ -1,22 +1,41 @@
+from enum import Enum
 from typing import Final
 
 import numpy as np
 
-Sec: Final[float] = 1e9
-mS: Final[float] = 1e6
-uS: Final[float] = 1e3
-nS: Final[float] = 1.0
 
-Hz: Final[float] = 1e-9
-kHz: Final[float] = 1e-6
-MHz: Final[float] = 1e-3
-GHz: Final[float] = 1.0
+class Units(Enum):
+    Sec = 1e9
+    mS = 1e6
+    uS = 1e3
+    nS = 1.0
+    Hz = 1e-9
+    kHz = 1e-6
+    MHz = 1e-3
+    GHz = 1.0
+    BLOCK = 128.0  # ns
+    BLOCKs = BLOCK
+    WORD = 8.0  # ns
+    WORDs = WORD
+    RAD = 1.0
+    DEG = np.pi / 180.0
 
-BLOCK: Final[float] = 128.0  # ns
-BLOCKs: Final[float] = BLOCK
 
-WORD: Final[float] = 8.0  # ns
-WORDs: Final[float] = WORD
+Sec: Final[Units] = Units.Sec
+mS: Final[Units] = Units.mS
+uS: Final[Units] = Units.uS
+nS: Final[Units] = Units.nS
 
-RAD: Final[float] = 1.0
-DEG: Final[float] = np.pi / 180.0
+Hz: Final[Units] = Units.Hz
+kHz: Final[Units] = Units.kHz
+MHz: Final[Units] = Units.MHz
+GHz: Final[Units] = Units.GHz
+
+BLOCK: Final[Units] = Units.BLOCK
+BLOCKs: Final[Units] = Units.BLOCKs
+
+WORD: Final[Units] = Units.WORD
+WORDs: Final[Units] = Units.WORDs
+
+RAD: Final[Units] = Units.RAD
+DEG: Final[Units] = Units.DEG
