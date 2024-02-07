0195a849e58325b140bbb54c313b98b302cdc18f
Hidehisa SHIOMI <shiomi.hidehisa.qiqb@osaka-u.ac.jp>
2024-02-07 13:00:23 +0900

    Fix '__init__.py' probrem

--- qubecalib/qubecalib/__init__.py
+++ qubecalib/qubecalib/__init__.py
@@ -2,15 +2,16 @@
 
 __version__ = "2.0.2"
 
-from . import backendqube, pulse, ui
+from . import backendqube, pulse, qube, ui
 from .meas import Recv, Send
 from .qube import Qube
 
 __all__ = [
-    "Qube",
+    "qube",
     "backendqube",
     "pulse",
     "ui",
+    "Qube",
     "Send",
     "Recv",
 ]
