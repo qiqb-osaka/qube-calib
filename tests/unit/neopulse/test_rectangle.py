1adec3ced2a36425410924179f964c24c145bb85
Akinori Machino <@amachino>
2024-04-24 17:59:56 +0900

    Change waveform syntax

--- tests/unit/neopulse/test_rectangle.py
+++ tests/unit/neopulse/test_rectangle.py
@@ -13,37 +13,38 @@ def test_empty_init():
     wf = Rectangle()
     assert wf.__iq__ is None
     assert wf.duration is None
-    assert wf.cmag == 1 + 0j  # TODO
+    assert wf.amplitude == 1.0
 
 
 def test_init():
     """Rectangle should initialize with arguments."""
-    wf = 0.5j * Rectangle(duration=10.0)
+    wf = Rectangle(duration=10.0, amplitude=0.5)
     assert wf.__iq__ is None
     assert wf.duration == 10.0
-    assert wf.cmag == 0 + 0.5j  # TODO
+    assert wf.amplitude == 0.5
 
 
 def test_func():
     """Rectangle should return the correct values."""
     duration = 10
-    wf = 0.1 * Rectangle(duration=duration)
+    wf = Rectangle(duration=duration, amplitude=0.1)
     assert wf.func(-5.0) == 0.0
-    assert wf.func(0.0) == 1.0
-    assert wf.func(5.0) == 1.0
-    assert wf.func(10.0) == 1.0
+    assert wf.func(0.0) == 0.1
+    assert wf.func(5.0) == 0.1
+    assert wf.func(10.0) == 0.0
     assert wf.func(15.0) == 0.0
 
 
 def test_ufunc():
     """Rectangle should return the correct samples."""
     duration = 8
-    wf = 2 * Rectangle(duration=duration)
+    amplitude = 2
+    wf = Rectangle(duration=duration, amplitude=amplitude)
 
     wf.begin = 0
     samples_begin0 = wf.ufunc(np.array([0, 2, 4, 6, 8, 10]))
-    assert samples_begin0 == pytest.approx([2, 2, 2, 2, 2, 0])
+    assert samples_begin0 == pytest.approx([2, 2, 2, 2, 0, 0])
 
     wf.begin = 2
     samples_begin2 = wf.ufunc(np.array([0, 2, 4, 6, 8, 10]))
-    assert samples_begin2 == pytest.approx([0, 2, 2, 2, 2, 2])
+    assert samples_begin2 == pytest.approx([0, 2, 2, 2, 2, 0])
