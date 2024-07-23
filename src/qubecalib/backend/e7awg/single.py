faaf90e6393f3385f3e715ad733084ff1d98a00e
Hidehisa SHIOMI <shiomi.hidehisa.qiqb@osaka-u.ac.jp>
2024-07-23 17:25:58 +0900

    Add multi.Driver and Fix schedule

--- src/qubecalib/backend/e7awg/single.py
+++ src/qubecalib/backend/e7awg/single.py
@@ -31,6 +31,10 @@ DEFAULT_NUM_WORKERS: Final[int] = 4
 DEFAULT_CAPTURE_TIMEOUT: Final[float] = 60.0
 
 
+def get_logger() -> logging.Logger:
+    return LOGGER
+
+
 # from quelware
 class CaptureReturnCode(Enum):
     CAPTURE_TIMEOUT = 1
@@ -66,7 +70,6 @@ class AwgSetting(E7Setting):
 class CapuSetting(E7Setting):
     capu: CaptureUnit
     cprm: CaptureParam
-    # trigger: Optional[AWG] = None
 
 
 @dataclass
@@ -79,14 +82,16 @@ class Driver:
     def __init__(
         self,
         ipaddr: IPv4Address | IPv6Address,
-        settings: list[E7Setting] = [],
+        settings: list[E7Setting],
     ) -> None:
-        self._executor = ThreadPoolExecutor(max_workers=DEFAULT_NUM_WORKERS)
+        self._ipaddr = str(ipaddr)
+        self._awgctrl = AwgCtrl(self._ipaddr)
+        self._capctrl = CaptureCtrl(self._ipaddr)
         self._awgctrl_lock = threading.Lock()
         self._capctrl_lock = threading.Lock()
-        self._ipaddr = str(ipaddr)
-        self._awgctrl: Final[AwgCtrl] = AwgCtrl(self._ipaddr)
-        self._capctrl: Final[CaptureCtrl] = CaptureCtrl(self._ipaddr)
+        self._executor = ThreadPoolExecutor(max_workers=DEFAULT_NUM_WORKERS)
+        self._settings = settings
+
         self._capus: Final[list[CaptureUnit]] = [
             s.capu for s in settings if isinstance(s, CapuSetting)
         ]
@@ -100,6 +105,26 @@ class Driver:
         self._initialize()
         self._load(settings)
 
+    @property
+    def awgctrl(self) -> AwgCtrl:
+        return self._awgctrl
+
+    @property
+    def capctrl(self) -> CaptureCtrl:
+        return self._capctrl
+
+    @property
+    def awgctrl_lock(self) -> threading.Lock:
+        return self._awgctrl_lock
+
+    @property
+    def capctrl_lock(self) -> threading.Lock:
+        return self._capctrl_lock
+
+    @property
+    def awgs(self) -> list[AWG]:
+        return self._awgs
+
     def _initialize(self) -> None:
         with self._capctrl_lock:
             self._capctrl.initialize(*self._capus)
@@ -108,10 +133,33 @@ class Driver:
             self._awgctrl.terminate_awgs(*self._awgs)
             self._awgctrl.clear_awg_stop_flags(*self._awgs)
 
-    def start(self) -> Results:
-        future = self.capture_at()
-        self.emit_now()
-        return future.result()
+    def _load(self, settings: list[E7Setting]) -> None:
+        for setting in settings:
+            if isinstance(setting, AwgSetting):
+                with self._awgctrl_lock:
+                    self._awgctrl.set_wave_sequence(setting.awg, setting.wseq)
+            elif isinstance(setting, CapuSetting):
+                with self._capctrl_lock:
+                    self._capctrl.set_capture_params(setting.capu, setting.cprm)
+            elif isinstance(setting, TriggerSetting):
+                pass
+            else:
+                raise ValueError(f"Unknown setting type: {setting}")
+        for setting in self._settings:
+            if isinstance(setting, TriggerSetting):
+                with self._capctrl_lock:
+                    self._capctrl.select_trigger_awg(setting.capm, setting.awg)
+                for u in CaptureModule.get_units(setting.capm):
+                    if u in self._capus:
+                        self._capus_with_triggers.append(u)
+
+    def start(self, timeout: float = DEFAULT_CAPTURE_TIMEOUT) -> Results:
+        with self._capctrl_lock:
+            self.capture_at()  # or self.capture_now() for individual capture
+            future = self.wait_until_capture_finishes(timeout)
+            self.emit_now()
+            result = future.result()
+        return result
 
     def abort(self) -> None:  # for capture
         # TODO implement
@@ -124,29 +172,31 @@ class Driver:
             self._awgctrl.terminate_awgs(*awgs)
             self._awgctrl.clear_awg_stop_flags(*awgs)
 
-    def capture_at(self) -> Future:
-        """Caputre data when trigger is received"""
-        with self._capctrl_lock:
-            # Capture data when trigger is received
-            self._capctrl.enable_start_trigger(
-                *[u for u in self._capus if u in self._capus_with_triggers]
-            )
-            # Capture data immediately for units without trigger
-            self._capctrl.start_capture_units(
-                *[u for u in self._capus if u not in self._capus_with_triggers]
-            )
-        return self._executor.submit(
-            self._capture_thread,
+    def capture_at(self) -> None:
+        """Caputre data when trigger is received. Locked capctrl is assumed."""
+        # Capture data when trigger is received
+        self._capctrl.enable_start_trigger(
+            *[u for u in self._capus if u in self._capus_with_triggers]
+        )
+        # Capture data immediately for units without trigger
+        capus = [u for u in self._capus if u not in self._capus_with_triggers]
+        self._capctrl.start_capture_units(*capus)
+        LOGGER.debug(
+            f"Capture units {', '.join([str(u) for u in capus])} started immediately"
         )
 
-    def capture_now(self) -> Results:
-        """Capture data immediately without waiting for the trigger"""
+    def capture_now(self) -> None:
+        """Capture data immediately without waiting for the trigger. Unlocked capctrl is required."""
         with self._capctrl_lock:
             self._capctrl.start_capture_units(self._capus)
-        future = self._executor.submit(
+
+    def wait_until_capture_finishes(
+        self, timeout: float = DEFAULT_CAPTURE_TIMEOUT
+    ) -> Future[Results]:
+        return self._executor.submit(
             self._capture_thread,
+            timeout,
         )
-        return future.result()
 
     def emit_now(self) -> None:
         with self._awgctrl_lock:
@@ -156,38 +206,36 @@ class Driver:
     def _check_capture_units_done(
         self,
     ) -> bool:
-        with self._capctrl_lock:
-            for capu in self._capus:
-                val = self._capctrl._CaptureCtrl__reg_access.read_bits(
-                    CaptureCtrlRegs.Addr.capture(capu),
-                    CaptureCtrlRegs.Offset.STATUS,
-                    CaptureCtrlRegs.Bit.STATUS_DONE,
-                    1,
-                )
-                if val == 0:
-                    return False
-            else:
-                return True
+        for capu in self._capus:
+            val = self._capctrl._CaptureCtrl__reg_access.read_bits(
+                CaptureCtrlRegs.Addr.capture(capu),
+                CaptureCtrlRegs.Offset.STATUS,
+                CaptureCtrlRegs.Bit.STATUS_DONE,
+                1,
+            )
+            if val == 0:
+                return False
+        else:
+            return True
 
     # from quelware
     def _clear_capture_unit_done(
         self,
     ) -> None:
-        with self._capctrl_lock:
-            for capu in self._capus:
-                for v in (0, 1):
-                    self._capctrl._CaptureCtrl__reg_access.write_bits(
-                        CaptureCtrlRegs.Addr.capture(capu),
-                        CaptureCtrlRegs.Offset.CTRL,
-                        CaptureCtrlRegs.Bit.CTRL_DONE_CLR,
-                        1,
-                        v,
-                    )
+        for capu in self._capus:
+            for v in (0, 1):
+                self._capctrl._CaptureCtrl__reg_access.write_bits(
+                    CaptureCtrlRegs.Addr.capture(capu),
+                    CaptureCtrlRegs.Offset.CTRL,
+                    CaptureCtrlRegs.Bit.CTRL_DONE_CLR,
+                    1,
+                    v,
+                )
 
     # from quelware
     def _wait_for_capture_data(
         self,
-        timeout: float,
+        timeout: float = DEFAULT_CAPTURE_TIMEOUT,
     ) -> bool:
         # waiting for the completion of captureing
         polling_interval = min(max(0.01, timeout / 1000.0), 0.1)
@@ -211,14 +259,13 @@ class Driver:
     def _check_capture_error(
         self,
     ) -> bool:
-        with self._capctrl_lock:
-            errdict: dict[int, list[Any]] = self._capctrl.check_err(*self._capus)
-            errflag = False
-            for capu, errlist in errdict.items():
-                for err in errlist:
-                    LOGGER.warning(f"capture unit {self._capus[capu]}: {err}")
-                    errflag = True
-            return errflag
+        errdict: dict[int, list[Any]] = self._capctrl.check_err(*self._capus)
+        errflag = False
+        for capu, errlist in errdict.items():
+            for err in errlist:
+                LOGGER.warning(f"capture unit {self._capus[capu]}: {err}")
+                errflag = True
+        return errflag
 
     # from quelware
     def _capture_thread(
@@ -235,24 +282,6 @@ class Driver:
             results.results[capu] = self._retrieve_capture_data(capu)
         return results
 
-    def _load(self, settings: list[E7Setting]) -> None:
-        with self._capctrl_lock, self._awgctrl_lock:
-            for setting in settings:
-                if isinstance(setting, AwgSetting):
-                    self._awgctrl.set_wave_sequence(setting.awg, setting.wseq)
-                elif isinstance(setting, CapuSetting):
-                    self._capctrl.set_capture_params(setting.capu, setting.cprm)
-                elif isinstance(setting, TriggerSetting):
-                    pass
-                else:
-                    raise ValueError(f"Unknown setting type: {setting}")
-            for setting in settings:
-                if isinstance(setting, TriggerSetting):
-                    self._capctrl.select_trigger_awg(setting.capm, setting.awg)
-                    for u in CaptureModule.get_units(setting.capm):
-                        if u in self._capus:
-                            self._capus_with_triggers.append(u)
-
     def _retrieve_capture_data(
         self,
         capu: CaptureUnit,
@@ -260,63 +289,62 @@ class Driver:
         status: CaptureReturnCode = CaptureReturnCode.SUCCESS
         cprm = self._cprms_by_capus[capu]
         n_sample_expected = cprm.calc_capture_samples()
-        with self._capctrl_lock:
-            n_sample_captured = self._capctrl.num_captured_samples(capu)
-            if n_sample_captured == n_sample_expected:
-                LOGGER.debug(
-                    f"the capture unit {self._ipaddr}:{capu} captured {n_sample_captured} samples"
-                )
-            else:
-                # TODO: investigate the reason this happens
-                LOGGER.warning(
-                    "the capture unit {self._ipaddr}:{capu} captured {n_sample_captured} samples, "
-                    "should be {n_sample_expected} samples"
-                )
-                status = CaptureReturnCode.BROKEN_DATA
+        n_sample_captured = self._capctrl.num_captured_samples(capu)
+        if n_sample_captured == n_sample_expected:
+            LOGGER.debug(
+                f"the capture unit {self._ipaddr}:{capu} captured {n_sample_captured} samples"
+            )
+        else:
+            # TODO: investigate the reason this happens
+            LOGGER.warning(
+                "the capture unit {self._ipaddr}:{capu} captured {n_sample_captured} samples, "
+                "should be {n_sample_expected} samples"
+            )
+            status = CaptureReturnCode.BROKEN_DATA
 
-            if DspUnit.CLASSIFICATION in cprm.dsp_units_enabled:
-                d = np.array(
-                    list(
-                        self._capctrl.get_classification_results(
-                            capu,
-                            n_sample_captured,
-                        )
-                    ),
-                    dtype=np.int16,
-                )
-            else:
-                c = np.array(
-                    self._capctrl.get_capture_data(
+        if DspUnit.CLASSIFICATION in cprm.dsp_units_enabled:
+            d = np.array(
+                list(
+                    self._capctrl.get_classification_results(
                         capu,
                         n_sample_captured,
-                    ),
-                    dtype=np.float32,
-                )
-                d = c[:, 0] + c[:, 1] * 1j
-
-            if DspUnit.INTEGRATION in cprm.dsp_units_enabled:
-                d = d.reshape(1, -1)
-            else:
-                d = d.reshape(cprm.num_integ_sections, -1)
-
-            if DspUnit.SUM in cprm.dsp_units_enabled:
-                e = np.hsplit(d, list(range(len(cprm.sum_section_list))[1:]))
-            else:
-                f = np.hsplit(
-                    d,
-                    np.cumsum(
-                        np.array(
-                            [
-                                w
-                                if DspUnit.DECIMATION not in cprm.dsp_units_enabled
-                                else int(w / 4)
-                                for w, _ in cprm.sum_section_list[:-1]
-                            ]
-                        )
                     )
-                    * cprm.NUM_SAMPLES_IN_ADC_WORD,
+                ),
+                dtype=np.int16,
+            )
+        else:
+            c = np.array(
+                self._capctrl.get_capture_data(
+                    capu,
+                    n_sample_captured,
+                ),
+                dtype=np.float32,
+            )
+        d = c[:, 0] + c[:, 1] * 1j
+
+        if DspUnit.INTEGRATION in cprm.dsp_units_enabled:
+            d = d.reshape(1, -1)
+        else:
+            d = d.reshape(cprm.num_integ_sections, -1)
+
+        if DspUnit.SUM in cprm.dsp_units_enabled:
+            e = np.hsplit(d, list(range(len(cprm.sum_section_list))[1:]))
+        else:
+            f = np.hsplit(
+                d,
+                np.cumsum(
+                    np.array(
+                        [
+                            w
+                            if DspUnit.DECIMATION not in cprm.dsp_units_enabled
+                            else int(w / 4)
+                            for w, _ in cprm.sum_section_list[:-1]
+                        ]
+                    )
                 )
-                e = [o.transpose() for o in f]
+                * cprm.NUM_SAMPLES_IN_ADC_WORD,
+            )
+            e = [o.transpose() for o in f]
         return Result(
             status=status,
             data=e,
@@ -324,7 +352,13 @@ class Driver:
 
     # TODO: direct access to the register will be implemented in the future
     def _get_dsp_units_enabled(self, capu: CaptureUnit) -> list[DspUnit]:
+        """Locked capctrl is assumed."""
         base_addr = CaptureParamRegs.Addr.capture(capu)
-        return self._capctrl._CaptureCtrl__reg_access.read(
+        result = self._capctrl._CaptureCtrl__reg_access.read(
             base_addr, CaptureParamRegs.Offset.DSP_MODULE_ENABLE
         )
+        return result
+
+    def clear_before_starting_emission(self) -> None:
+        """Locked awgctrl is assumed."""
+        self._awgctrl.clear_awg_stop_flags(*self._awgs)
