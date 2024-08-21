8b30ce66bc139caaa224f21b67af1acb02524ebb
Hidehisa SHIOMI <shiomi.hidehisa.qiqb@osaka-u.ac.jp>
2024-08-21 09:40:41 +0900

    Release boxpool at the end of measurement to free up resources

--- src/qubecalib/qubecalib.py
+++ src/qubecalib/qubecalib.py
@@ -1786,6 +1786,8 @@ class Executor:
         # ワークキューが空になったら実行を止める
         if not self._work_queue:
             self.check_config()
+            self._boxpool = BoxPool()
+            self.clear_log()
             raise StopIteration()
         # Sequencer が見つかるまでコマンドを逐次実行
         while True:
