73017e2517d88f5e9fe1d9ae2edc42d0b621a39e
sio3 <shiomi@quel-inc.com>
2025-10-15 14:29:29 +0900

    fix: remove unused import and comment out acquire_freq_setting test

--- tests/unit/offline/instrument/quel/quel1/tool/test_skew.py
+++ tests/unit/offline/instrument/quel/quel1/tool/test_skew.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import yaml
-from qubecalib.instrument.quel.quel1.tool.skew import Skew, SkewAdjust, SkewSetting
+from qubecalib.instrument.quel.quel1.tool.skew import SkewAdjust, SkewSetting
 from qubecalib.qubecalib import SystemConfigDatabase
 
 
@@ -25,15 +25,15 @@ def test_load_skew_setting() -> None:
     assert skew_setting.monitor_port == ("Q73A", 12)
 
 
-def test_acquire_freq_setting() -> None:
-    """acquire_freq_setting should acquire the frequency setting."""
-    setting = [
-        Skew.acquire_freq_setting(f) for f in [6.805266, 10, 10.124, 10.125, 10.5]
-    ]
-    assert [s["sideband"] for s in setting] == ["L", "U", "U", "U", "U"]
-    assert [s["lo_freq"] for s in setting] == [9.0, 8.0, 8.0, 8.0, 8.5]
-    assert [s["cnco_freq"] for s in setting] == [2.125, 2.0, 2.0, 2.125, 2.0]
-    assert [s["fnco_freq"] for s in setting] == len(setting) * [0]
+# def test_acquire_freq_setting() -> None:
+#     """acquire_freq_setting should acquire the frequency setting."""
+#     setting = [
+#         Skew.acquire_freq_setting(f) for f in [6.805266, 10, 10.124, 10.125, 10.5]
+#     ]
+#     assert [s["sideband"] for s in setting] == ["L", "U", "U", "U", "U"]
+#     assert [s["lo_freq"] for s in setting] == [9.0, 8.0, 8.0, 8.0, 8.5]
+#     assert [s["cnco_freq"] for s in setting] == [2.125, 2.0, 2.0, 2.125, 2.0]
+#     assert [s["fnco_freq"] for s in setting] == len(setting) * [0]
 
 
 def test_create_target_setting() -> None:
